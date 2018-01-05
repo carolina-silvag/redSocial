c341dd32112c5dc12cc9151dad12f61b5db984de
carolina-silvag <carolinasguzman@gmail.com>
2018-01-05 11:36:06 -0300

    borra codigo que no se ocupa

--- scripts/main.js
+++ scripts/main.js
@@ -19,19 +19,12 @@ function FriendlyChat() {
   this.signInSnackbar = document.getElementById('must-signin-snackbar');
   this.userOnline = document.getElementById('cont-user');
 
-  $('#oculto').hide();
-  $('#oculto2').hide();
+  
 
 
   //Guarda el mensaje en el envío del formulario
-  this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
   this.signOutButton.addEventListener('click', this.signOut.bind(this));
   // this.signInButton.addEventListener('click', this.signIn.bind(this));
-
-  //Alternar para el botón
-  var buttonTogglingHandler = this.toggleButton.bind(this);
-  this.messageInput.addEventListener('keyup', buttonTogglingHandler);
-  this.messageInput.addEventListener('change', buttonTogglingHandler);
 
   // Events for image upload.
   this.submitImageButton.addEventListener('click', function(e) {
@@ -361,10 +354,6 @@ FriendlyChat.prototype.displayMessage = function(key, name, text, picUrl, photoI
     // Replace all line breaks by <br>.
     messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
   }
-  // Show the card fading-in.
-  setTimeout(function() {div.classList.add('visible')}, 1);
-  this.messageList.scrollTop = this.messageList.scrollHeight;
-  this.messageInput.focus();
 };
 
 // Enables or disables the submit button depending on the values of the input
@@ -392,91 +381,11 @@ window.onload = function() {
 
 };
 
-/*agregar usuarios activos*/
 
 
-
-/*function UserConectados(name, uid) {
-  var li = '<li id="'+uid+'" class="mdl-list__item><span class="mdl-lidt__item-primary_content><i class="material-icons mdl-list__item-icon">person</i>'
-            +name+ '</span></li>'
-  $('#user-online').append(li);
-}*/
-
-
-/*agregar comentario a la imagen*/
-function enabled() {
-  var comments = document.getElementById('comment').value;
-  var cantidad = 140 - comments.length;
-  var espacios = comments.split(' ').length - 1;
-  var enters = comments.split('\n').length - 1;
-  document.getElementById('p1').innerHTML=cantidad;
-
-  if (comments.length == 0 || cantidad < 0 || espacios + enters == comments.length){
-    document.getElementById('btn').disabled = true;
-    document.getElementById('btn').className = 'btn-disabled';
-  }else{
-    document.getElementById('btn').disabled = false;
-    document.getElementById('btn').className = 'btn-enabled';
-  }
-}
-function add(){
-  /*tomo texto de textareaa*/
-  var comments= document.getElementById('comment').value;
-  /*limpealo del textarea*/
-  document.getElementById('comment').value = '';
-  /*el div en donde estan los comentario el html*/
-  var newComments= document.createElement('div');
-  var cont = document.getElementById('cont');
-  var hora =moment().format('LT');
-
-  var paragraph= document.createElement('p');
-  var nodoText=document.createTextNode(comments);
-  var nodoHour=document.createTextNode(hora);
-  paragraph.appendChild(nodoText);
-  /*agregar todos los huerfanos al padre(contenedor)*/
-  newComments.appendChild(paragraph);
-  newComments.appendChild(nodoHour);
-  cont.appendChild(newComments);
-  /*volver a preguntar por cada comentario*/
-  enabled();
-}
-function numColor() {
-  var comments = document.getElementById('comment').value;
-  var cantidad = 140 - comments.length;
-  if (cantidad < 20 && cantidad >=10){
-    document.getElementById('tell').className = 'tell-2';
-  }else if(cantidad < 10 && cantidad >=0){
-    document.getElementById('tell').className = 'tell-3';
-  }else{
-    document.getElementById('tell').className = 'tell-1';
-  }
-}
-
-function agrandar(){
-  var comments = document.getElementById('comment').value;
-  var linea = comments.split('\n');
-  var lineas_necesarias = parseInt(comments.length/37) + 1;
-    if((linea.length - 1 + lineas_necesarias) > 3){
-      document.getElementById('comment').rows = linea.length - 1 + lineas_necesarias;
-    } else {
-      document.getElementById('comment').rows = 3;  
-    }
-}
-
-function enabled() {
-  var comments = document.getElementById('comment').value;
-  var cantidad = 140 - comments.length;
-  document.getElementById('p1').innerHTML=cantidad;
-  if (comments.length == 0 || cantidad < 0){
-    document.getElementById('btn').disabled = true;
-  }else{
-    document.getElementById('btn').disabled = false;
-  }
-}
 
 /*mapa*/
 
-/*falta localizar por filtro*/
 var map;
 var map2;
 var service;
